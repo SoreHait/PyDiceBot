5a8620d6a119982a0de878b6bb777e7c69eb3268
SoreHait <45304050@qq.com>
2020-01-29 21:01:26 +0800

    优化数据库操作

--- applications/setmode.py
+++ applications/setmode.py
@@ -4,20 +4,27 @@ import sqlite3
 @on_command('set', only_to_me=False)
 async def setmode(session: CommandSession):
     mode = session.get('mode')
-    gid = session.get('groupid')
-    db = sqlite3.connect('.//data//group.db')
-    c = db.cursor()
-    c.execute(f"SELECT GID FROM DATA WHERE GID={gid}")
-    if len(c.fetchall()) == 0:
-        c.execute(f"INSERT INTO DATA (GID,MODE) VALUES ('{gid}','{mode}')")
+    if (mode in ['coc','dnd']) or (str(type(mode)) == "<class 'int'>" and 1 <= mode <= 1000):
+        gid = session.ctx['group_id']
+        db = sqlite3.connect(f'.//data//{gid}.db')
+        c = db.cursor()
+        c.execute(f"SELECT COUNT(*) FROM sqlite_master WHERE TYPE='table' AND NAME='MODE'")
+        if c.fetchone()[0] == 0:
+            c.execute(f"CREATE TABLE MODE (MODE TEXT PRIMARY KEY NOT NULL)")
+            c.execute(f"INSERT INTO MODE (MODE) VALUES ('{mode}')")
+        else:
+            c.execute(f"DELETE FROM MODE")
+            c.execute(f"INSERT INTO MODE (MODE) VALUES ('{mode}')")
+        db.commit()
+        db.close()
+        await session.send('设定成功')
     else:
-        c.execute(f"UPDATE DATA SET MODE='{mode}' WHERE GID={gid}")
-    db.commit()
-    db.close()
-    await session.send('设定成功')
+        await session.send('请将模式设定为coc，dnd或1-1000范围内的整数')
 
 @setmode.args_parser
 async def _(session: CommandSession):
-    session.state['groupid'] = session.ctx['group_id']
-    session.state['mode'] = session.current_arg_text
+    try:
+        session.state['mode'] = int(session.current_arg_text)
+    except Exception:
+        session.state['mode'] = session.current_arg_text.lower()
     return

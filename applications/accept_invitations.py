5559ec77e455dbba0f3cc4428862c1b20db77ee1
SoreHait <45304050@qq.com>
2020-02-04 19:13:41 +0800

    自动接受邀请请求

---
+++ applications/accept_invitations.py
@@ -0,0 +1,20 @@
+from nonebot import on_request, RequestSession
+
+accept = False
+whitelist_group = [
+    1011692502
+]
+whitelist_user = [
+
+]
+
+@on_request('group')
+async def _(session: RequestSession):
+    if session.ctx['sub_type'] == 'invite' and accept:
+        await session.approve()
+    elif session.ctx['sub_type'] == 'invite' and session.ctx['group_id'] in whitelist_group:
+        await session.approve()
+    elif session.ctx['sub_type'] == 'invite' and session.ctx['user_id'] in whitelist_user:
+        await session.approve()
+    else:
+        await session.reject()

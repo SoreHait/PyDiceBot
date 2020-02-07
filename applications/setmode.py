7f501c43fce56f9c6931e1a91ada911092109f3c
SoreHait <45304050@qq.com>
2020-02-08 04:43:48 +0800

    添加.set clr/show

--- applications/setmode.py
+++ applications/setmode.py
@@ -4,8 +4,31 @@ from app_addon.json_operations import dumpjson, loadjson
 @on_command('set', only_to_me=False)
 async def setmode(session: CommandSession):
     mode = session.get('mode')
-    if (mode in ['coc','dnd']) or (str(type(mode)) == "<class 'int'>" and 1 <= mode <= 1000):
-        gid = session.ctx['group_id']
+    gid = session.ctx['group_id']
+    if mode == 'clr':
+        data = loadjson(gid)
+        try:
+            del data['mode']
+        except Exception:
+            pass
+        await session.send('默认规则清除成功')
+        dumpjson(gid, data)
+    elif mode == 'show':
+        data = loadjson(gid)
+        try:
+            currentmode = data['mode']
+        except KeyError:
+            await session.send('当前没有设定规则，默认使用coc规则')
+            return
+        try:
+            currentmode = int(currentmode)
+        except Exception:
+            pass
+        if currentmode in ['coc','dnd']:
+            await session.send(f'当前规则是{currentmode}规则')
+        else:
+            await session.send(f'当前默认骰是{currentmode}')
+    elif (mode in ['coc','dnd']) or (str(type(mode)) == "<class 'int'>" and 1 <= mode <= 1000):
         data = loadjson(gid)
         data['mode'] = mode
         dumpjson(gid, data)
@@ -15,6 +38,10 @@ async def setmode(session: CommandSession):
 
 @setmode.args_parser
 async def _(session: CommandSession):
+    if session.current_arg_text == 'show':
+        session.state['mode'] = 'show'
+    elif session.current_arg_text == 'clr':
+        session.state['mode'] = 'clr'
     try:
         session.state['mode'] = int(session.current_arg_text)
     except Exception:

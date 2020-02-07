8d85737a231aacb3436f799dccad4a92db324f1e
SoreHait <45304050@qq.com>
2020-02-08 05:18:05 +0800

    修复bug

--- applications/help.py
+++ applications/help.py
@@ -1,11 +1,11 @@
 from nonebot import on_command, CommandSession
-from app_addon.json_operations import loadjson
+from json import load
 
 @on_command('help', only_to_me=False)
 async def help(session: CommandSession):
     keyword = session.get('keyword')
     with open('.//app_addon//helpPages.json', 'r', encoding='utf-8') as f:
-        helpPages = loadjson(f)
+        helpPages = load(f)
     try:
         helpText = helpPages[keyword]
     except KeyError:

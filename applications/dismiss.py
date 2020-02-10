13e9ba9762589b7e9a7557eb01076bc9369735a9
SoreHait <45304050@qq.com>
2020-02-10 09:46:49 +0800

    添加群主/管理验证

--- applications/dismiss.py
+++ applications/dismiss.py
@@ -5,9 +5,13 @@ from os import path, remove
 @on_command('dismiss', only_to_me=False)
 async def dismiss(session: CommandSession):
     gid = int(session.ctx['group_id'])
-    bot = session.bot
-    await session.send('正在清理本群数据库')
-    if path.exists(f'.//data//{gid}.json'):
-        remove(f'.//data//{gid}.json')
-    await session.send('bot将退出群聊')
-    await bot.set_group_leave(group_id=gid)
+    role = session.ctx['sender']['role']
+    if role in ['owner', 'admin']:
+        bot = session.bot
+        await session.send('正在清理本群数据库')
+        if path.exists(f'.//data//{gid}.json'):
+            remove(f'.//data//{gid}.json')
+        await session.send('bot将退出群聊')
+        await bot.set_group_leave(group_id=gid)
+    else:
+        await session.send('只有管理员或群主才可使用此命令')

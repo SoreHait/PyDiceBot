6df762ecbbfafd6387dc700715d39c48354f704c
SoreHait <45304050@qq.com>
2020-02-11 11:21:47 +0800

    .ri [调整值]

--- applications/initiative.py
+++ applications/initiative.py
@@ -5,6 +5,13 @@ import random
 
 @on_command('ri')
 async def rollInitiative(session: CommandSession):
+    shift = session.get('shift')
+    try:
+        shiftint = int(shift)
+    except Exception:
+        if shift != '':
+            await session.send('请输入一个加/减法算式')
+            return
     uid = str(session.ctx['sender']['user_id'])
     gid = session.ctx['group_id']
     data = loadjson(gid)
@@ -21,10 +28,26 @@ async def rollInitiative(session: CommandSession):
     except Exception:
         data['initName'] = {}
     init = random.randint(1, 20)
-    data['initiative'][uid] = init
     data['initName'][uid] = nickname
+    if shift == '':
+        data['initiative'][uid] = init
+        await session.send(f'{nickname}的先攻骰点：\nD20={init}')
+    else:
+        data['initiative'][uid] = init + shiftint
+        if str(shiftint) == shift and not '-' in shift:
+            await session.send(f'{nickname}的先攻骰点：\nD20+调整值={str(init)}+{shift}={init+shiftint}')
+        else:
+            await session.send(f'{nickname}的先攻骰点：\nD20+调整值={str(init)}{shift}={init+shiftint}')
     dumpjson(gid, data)
-    await session.send(f'{nickname}的先攻骰点：\nD20={init}')
+
+
+@rollInitiative.args_parser
+async def _(session: CommandSession):
+    if session.current_arg_text == '':
+        session.state['shift'] = ''
+    elif session.current_arg_text != '':
+        session.state['shift'] = session.current_arg_text
+    return
 
 
 @on_command('init')

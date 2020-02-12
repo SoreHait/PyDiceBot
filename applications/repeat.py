72b083d9a68b9370d9d01b5e037c1b426ed9e5ec
SoreHait <45304050@qq.com>
2020-02-12 14:26:50 +0800

    取消set改为直接跟数字，现在可以用小数

--- applications/repeat.py
+++ applications/repeat.py
@@ -1,78 +1,58 @@
 from nonebot import on_command, CommandSession
 from nonebot import on_natural_language, NLPSession, IntentCommand
 from app_addon.json_operations import loadjson, dumpjson
-from random import randint
+from random import random
 
 
-@on_command('rep', only_to_me=False)
+@on_command('rep', aliases=('fudu', 'fd', '复读',), only_to_me=False)
 async def repeat(session: CommandSession):
-    mode = session.get('mode')
-    if mode == 'None':
-        await session.send('请输入参数，请通过.help rep查看帮助')
-    elif mode == 'Else':
-        await session.send('未知参数，请通过.help rep查看帮助')
-    elif mode == 'show':
-        gid = session.ctx['group_id']
-        data = loadjson(gid)
-        try:
-            rate = data['repRate']
-        except Exception:
-            await session.send('当前复读几率为0%')
-            return
-        await session.send(f'当前复读机率为{rate}%')
-    elif mode == 'set':
-        role = session.ctx['sender']['role']
-        if role not in ['owner', 'admin']:
-            await session.send('只有管理员或群主才可使用此命令')
-            return
-        rate = session.get('rate')
-        try:
-            rate = int(rate)
-        except Exception:
-            await session.send('请输入一个在0-100之间的数')
-            return
-        if 0 <= rate <= 100:
+    arg = session.current_arg_text
+    if arg == 'off':
+        arg = 0
+    try:
+        rate = float(arg)
+    except Exception:
+        if arg == '':
+            await session.send('请输入参数，请通过.help rep查看帮助')
+        elif arg == 'show':
             gid = session.ctx['group_id']
             data = loadjson(gid)
-            data['repRate'] = rate
-            dumpjson(gid, data)
-            await session.send('设定成功')
+            try:
+                rate = data['repRate']
+            except Exception:
+                await session.send('当前复读几率为0%')
+                return
+            await session.send(f'当前复读机率为{rate}%')
         else:
-            await session.send('请输入一个在0-100之间的数')
-
-
-@repeat.args_parser
-async def _(session: CommandSession):
-    mode = session.current_arg_text.split(' ')
-    if session.current_arg_text == '':
-        session.state['mode'] = 'None'
-    elif mode[0] == 'set':
-        try:
-            session.state['rate'] = mode[1]
-        except Exception:
-            session.state['mode'] = 'None'
-            return
-        session.state['mode'] = 'set'
-    elif mode[0] == 'show':
-        session.state['mode'] = 'show'
+            await session.send('未知参数，请通过.help rep查看帮助')
+        return
+    role = session.ctx['sender']['role']
+    if role not in ['owner', 'admin']:
+        await session.send('只有管理员或群主才可使用此命令')
+        return
+    if 0 <= rate <= 100:
+        gid = session.ctx['group_id']
+        data = loadjson(gid)
+        data['repRate'] = rate
+        dumpjson(gid, data)
+        await session.send('设定成功')
     else:
-        session.state['mode'] = 'Else'
-    return
+        await session.send('请输入一个在0-100之间的数')
 
 
 @on_command('sayagain')
 async def sayagain(session: CommandSession):
-    text = session.current_arg
     gid = session.ctx['group_id']
     data = loadjson(gid)
     try:
-        rate = int(data['repRate'])
+        rate = float(data['repRate']/100)
     except Exception:
         return
-    if rate == 0:
+    if rate == 0.0:
         return
     else:
-        if randint(1, 100) <= rate:
+        text = session.current_arg
+        if random() <= rate:
             await session.send(text)
 
 
